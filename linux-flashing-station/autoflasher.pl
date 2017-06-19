781a35941f0f122e7a5db9d5fb71a8882138c4cc
Jesse Vincent <jesse@keyboard.io>
2017-06-19 13:05:40 +0800

    Automatically use three connected avrtinyisps.
    probe them by parsing lsusb
    
    simplify debugging output
    
    avrdude has a double free on exit when flashing the attiny. use a really big hammer (env variable) to work around it.

--- linux-flashing-station/autoflasher.pl
+++ linux-flashing-station/autoflasher.pl
@@ -18,25 +18,56 @@ my $fuses = {
       "-e -Ulock:w:0x3F:m -Uefuse:w:0xcb:m -Uhfuse:w:0xd8:m -Ulfuse:w:0xff:m"
 };
 
+
+sub program_boards {
+	my @usbtiny_devices = probe_devices();
+	if (scalar @usbtiny_devices != 3) {
+		print "ERROR: I only see ".(scalar @usbtiny_devices). "red programmers\n";
+		print "ERROR: ".(3-(scalar @usbtiny_devices))." are not here\n";
+		print "ERROR: Check USB cables\n";
+	        print "ERROR: Check switches on red boards\n";
+	} 
+	my $boards_programmed = 0;
+	eval {	
+	for my $addr (@usbtiny_devices) {
+		print $boards_programmed+1 .": ";
+		program_board($addr);
+		$boards_programmed++;
+	}
+	};
+	if (my $msg = $@) {
+		print "ERROR: programmed $boards_programmed chips\n";	
+		die $msg;
+	} 
+	if ($boards_programmed < 3 ) {
+		die "Only programmed $boards_programmed chips\n";
+	} else {
+		print "\n\nDONE! All 3 chips programmed.\n";
+	}
+}
+
+
 sub program_board {
-    my $device = probe_device();
+    my $addr = shift;
+
+    my $device = probe_device($addr);
 
     # ATMega32u4 0x1e9587
     # ATTiny88 0x1e9311
     # none found: none;
 
     if ( $device =~ /0x1e9587/i ) {
-        print "\n\nI see ATMega32U4\n\n";
-        set_atmega_fuses();
-        flash_atmega_device();
-        print "OK! DONE! \n\n";
+        print "ATMega32U4...";
+        set_atmega_fuses($addr);
+        flash_atmega_device($addr);
+        print "\n";
 
     }
     elsif ( $device =~ /0x1e9311/i ) {
-        print "\n\nI see ATTiny88\n\n";
-        set_attiny_fuses();
-        flash_attiny_device();
-        print "OK! DONE!\n\n";
+        print "ATTiny88...  ";
+        set_attiny_fuses($addr);
+        flash_attiny_device($addr);
+        print "\n";
     }
     else {
         print "\n\nERROR: I do not see a chip\n\n";
@@ -45,20 +76,22 @@ sub program_board {
 }
 
 sub set_atmega_fuses {
-    print "Setting fuses...";
+    my $addr= shift;
+    print "Fuses...";
     my ( $output, $error, $exit ) =
-      run_avrdude( "atmega32u4", split( /\s+/, $fuses->{'atmega32u4'} ) );
+      run_avrdude( $addr,"atmega32u4", split( /\s+/, $fuses->{'atmega32u4'} ) );
     if ($exit) {
         error("could not set fuses on atmega");
     }
     else {
-        print "OK \n";
+        print "OK. ";
     }
 }
 
 sub flash_atmega_device {
-    print "Putting program on chip...";
-    my ( $output, $error, $exit ) = run_avrdude(
+	 my $addr = shift;
+    print "Program...";
+    my ( $output, $error, $exit ) = run_avrdude( $addr,
         "atmega32u4",                                    qw"-B 1",
         "-Uflash:w:" . $firmware->{'atmega32u4'} . ":i", qw"-Ulock:w:0x3F:m"
     );
@@ -66,32 +99,35 @@ sub flash_atmega_device {
         error("FAIL - flashing ATMega32u4: \n$error\n");
     }
     else {
-        print "OK \n";
+        print "OK. ";
     }
 }
 
 sub set_attiny_fuses {
-    print "Setting fuses...";
+ my $addr = shift;
+    print "Fuses...";
     my ( $output, $error, $exit ) =
-      run_avrdude( "attiny88", split( /\s+/, $fuses->{'attiny88'} ) );
+      run_avrdude( $addr, "attiny88", split( /\s+/, $fuses->{'attiny88'} ) );
 
     if ($exit) {
         error("FAIL - setting attiny88 fuses: \n$error\n");
     }
     else {
-        print "OK\n";
+        print "OK. ";
     }
 }
 
 sub flash_attiny_device {
-    print "Putting program on chip...";
-    my ( $output, $error, $exit ) = run_avrdude( "attiny88",
+    my $addr = shift;
+    print "Program...";
+    my ( $output, $error, $exit ) = run_avrdude( $addr, "attiny88",
         qw"-B 1 -U", "flash:w:" . $firmware->{'attiny88'} . ":i" );
     if ($exit) {
-        error("FAIL - flashing attiny88: \n$error\n");
+        error("FAIL - flashing attiny88");
+	#: \n$error\n");
     }
     else {
-        print "OK\n";
+        print "OK. ";
     }
 
 }
@@ -106,21 +142,21 @@ sub reset_usb_bus {
 }
 
 sub run_avrdude {
-    my $device  = shift;
+    my $addr = shift;
+    my $device  = shift; 
     my @command = @_;
     my ( $in, $out, $err, $exitcode );
 
-    reset_usb_bus();
+    $ENV{'MALLOC_CHECK_'} ='0';
+    my @cmd = ( 'avrdude', '-v', "-p$device", "-P$addr" , "-cusbtiny", "-q", @command );
 
-    my @cmd = ( 'avrdude', '-v', "-p$device", "-cusbtiny", "-q", @command );
-
-    #  print join(" ","\n",@cmd, "\n");
+#      print join(" ","\n",@cmd, "\n");
     eval {
         IPC::Run::run( \@cmd, \$in, \$out, \$err );
 
     };
     if ($@) {
-        warn($@);
+        $err .= $@;
 
         # Could not run the program
         $exitcode = undef;
@@ -140,8 +176,8 @@ sub run_avrdude {
 }
 
 sub probe_device {
-
-    my ( $output, $error, $exit ) = run_avrdude( "m8", "" );
+	my $addr = shift;
+    my ( $output, $error, $exit ) = run_avrdude( $addr, "m8", "" );
     if ( $error =~ /Device signature = (\S*)/ ) {
         my $chip = $1;
         return $chip;
@@ -165,12 +201,29 @@ sub prompt_to_start {
         ReadMode(0);
         if ( $nothing eq '_' ) { exit(0) }
         system("clear");
-        eval { program_board(); };
+        eval { program_boards(); };
         if ( my $msg = $@ ) {
-            warn "ERROR: $msg";
+            warn "\n\nERROR: $msg";
         }
     }
 
+}
+
+sub probe_devices {
+    reset_usb_bus();
+sleep(2);
+my ($in,$out,$err);
+my @data = `lsusb -d 1781:0c9f`;
+my @devices;
+for my $line ( @data) {
+	if ($line =~ /^Bus (\d+) Device (\d+)(.*?)/mi) {
+		my $bus = $1;
+		my $device = $2;
+		push @devices, "usb:$bus:$device";
+	} else {
+	}
+}
+	return @devices;
 }
 
 prompt_to_start();
