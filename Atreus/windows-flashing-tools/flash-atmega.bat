0458fecc2e6bb298860be24bcfc4e1a8a7867b0f
Jesse Vincent <jesse@keyboard.io>
2020-07-05 20:33:42 -0700

    make the windows flasher faster?

--- Atreus/windows-flashing-tools/flash-atmega.bat
+++ Atreus/windows-flashing-tools/flash-atmega.bat
@@ -2,7 +2,7 @@ bin\avrdude -v -patmega32u4 -cusbtiny >> attiny-log.txt 2>&1 || goto :wrong_chip
 
 bin\avrdude -patmega32u4 -v -cusbtiny  -e -Ulock:w:0x3F:m -Uefuse:w:0xcb:m -Uhfuse:w:0xd8:m -Ulfuse:w:0xff:m  >> atmega-log.txt 2>&1  || goto :error
 
-bin\avrdude -patmega32u4 -v -cusbtiny  -Uflash:w:firmware/atmega.hex:i -Ulock:w:0x2F:m  >> atmega-log.txt 2>&1 || goto :error
+bin\avrdude -patmega32u4 -v -cusbtiny -B 0.5  -Uflash:w:firmware/atmega.hex:i -Ulock:w:0x2F:m  >> atmega-log.txt 2>&1 || goto :error
 
 
 cscript bin\popup.vbs "OK: Flashed ATMega"
