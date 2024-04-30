42f3a69fec0a058c40201c2a35e5780de729c0af
Jesse Vincent <jesse@keyboard.io>
2024-04-29 20:08:36 -0700

    windows flasher update

--- Model-100/windows-flashing-station/flash-attiny88-chip.bat
+++ Model-100/windows-flashing-station/flash-attiny88-chip.bat
@@ -6,23 +6,27 @@ echo "%time% About to flash Model 100"
 echo "%time% Flashing ATTiny"
 
 :try_fuses
-for /L %%x in (1, 1, 4) do (
-    bin\avrdude -v -pattiny88 -cusbtiny -e -U lfuse:w:0xEE:m -U hfuse:w:0xDD:m -U efuse:w:0xFE:m -U lock:w:0x3F:m && goto try_flash
+for /L %%x in (1, 1, 6) do (
+    bin\avrdude -v -pattiny88 -cusbtiny  -B 2 -e -U lfuse:w:0xEE:m -U hfuse:w:0xDD:m -U efuse:w:0xFE:m -U lock:w:0x3F:m && goto try_flash
     echo "Attempt %%x to set fuses failed." >> attiny-log.txt
+    timeout /t 1 /nobreak >nul
+
 )
-echo "Failed to set fuses after 4 attempts." >> attiny-log.txt
+echo "Failed to set fuses after 6 attempts." >> attiny-log.txt
 cscript bin\popup.vbs "FAIL: Fuses could not be set"
 goto main_loop
 
 :try_flash
-for /L %%x in (1, 1, 4) do (
+for /L %%x in (1, 1, 6) do (
     bin\avrdude -v -pattiny88 -cusbtiny -u -V -B .5 -U flash:w:firmware/attiny88.hex:i && (
         echo "OK: Flashed ATTiny"
         goto main_loop
     )
     echo "Attempt %%x to flash ATTiny failed." >> attiny-log.txt
+    timeout /t 1 /nobreak >nul
+
 )
-echo "Failed to flash ATTiny after 4 attempts." >> attiny-log.txt
+echo "Failed to flash ATTiny after 6 attempts." >> attiny-log.txt
 cscript bin\popup.vbs "FAIL: Flashing could not be completed"
 goto main_loop
 
