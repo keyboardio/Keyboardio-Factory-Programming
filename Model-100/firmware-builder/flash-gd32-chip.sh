171be92f9faec38819726061496c11833bc043fa
Jesse Vincent <jesse@keyboard.io>
2024-04-29 19:33:13 -0700

    Build system updates for mp2
    - Fixed quoting issue in firmware version flag in `build-all` script
    - Excluded unnecessary files from source bundle in `build-all` script
    - Changed file mode to executable for `flash-attiny88-chip.sh` and `flash-gd32-chip.sh`
    - Added delays between commands in `flash-gd32-chip.sh` to improve reliability
    - Simplified and improved reliability of `flash-attiny88-chip.bat` with loop retries for flashing and setting fuses
    - Added logging and user feedback for errors in `flash-attiny88-chip.bat`

--- Model-100/firmware-builder/flash-gd32-chip.sh
+++ Model-100/firmware-builder/flash-gd32-chip.sh
@@ -3,7 +3,7 @@
 echo "Connect PCBA, then Hit ENTER to flash GD32 chip."
 read
 
-st-info --chipid  && \
-st-flash erase && \
-st-flash --format ihex write "./output/latest/gd32_firmware_with_bootloader.hex" && \
+st-info --chipid  && sleep 2 && \
+st-flash erase && sleep 2 &&\
+st-flash --format ihex write "./output/latest/gd32_firmware_with_bootloader.hex" && sleep 2 && \
 st-flash reset
--- Model-100/windows-flashing-station/flash-attiny88-chip.bat
+++ Model-100/windows-flashing-station/flash-attiny88-chip.bat
@@ -1,79 +1,33 @@
+@echo off
+:main_loop
+set /p DUMMY="Connect PCBA, then hit ENTER to flash ATTiny88 chip."
 
-:start
+echo "%time% About to flash Model 100"
+echo "%time% Flashing ATTiny"
 
-@set /p DUMMY=Connect PCBA, then Hit ENTER to flash ATTiny88 chip.
+:try_fuses
+for /L %%x in (1, 1, 4) do (
+    bin\avrdude -v -pattiny88 -cusbtiny -e -U lfuse:w:0xEE:m -U hfuse:w:0xDD:m -U efuse:w:0xFE:m -U lock:w:0x3F:m && goto try_flash
+    echo "Attempt %%x to set fuses failed." >> attiny-log.txt
+)
+echo "Failed to set fuses after 4 attempts." >> attiny-log.txt
+cscript bin\popup.vbs "FAIL: Fuses could not be set"
+goto main_loop
 
-@echo "%time% About to flash Model 100"
-
-
-@echo "%time% Flashing ATTiny"
-
-:attiny_1
-
-bin\avrdude -v -pattiny88 -cusbtiny   || goto :wrong_attiny_1
-bin\avrdude -v -pattiny88 -cusbtiny  -e -U lfuse:w:0xEE:m -U hfuse:w:0xDD:m -U efuse:w:0xFE:m -U lock:w:0x3F:m || goto :attiny_1_error
-bin\avrdude -v -pattiny88 -cusbtiny -u -V -B .5 -U flash:w:firmware/attiny88.hex:i || goto :attiny_1_error
-bin\avrdude -pattiny88 -cusbtiny -u -U flash:v:firmware/attiny88.hex:i || goto :attiny_try_2
-
-@echo "OK: Flashed ATTiny"
-
-@goto :start
-
-:attiny_try_2
-
-bin\avrdude -pattiny88 -cusbtiny -u -U flash:v:firmware/attiny88.hex:i || goto :attiny_try_3
-@echo "OK: Flashed ATTiny"
-
-@goto :start
-
-:attiny_try_3
-
-bin\avrdude -pattiny88 -cusbtiny -u -U flash:v:firmware/attiny88.hex:i || goto :attiny_try_4
-@echo "OK: Flashed ATTiny"
-
-@goto :start
-
-:attiny_try_4
-
-bin\avrdude -pattiny88 -cusbtiny -u -U flash:v:firmware/attiny88.hex:i || goto :attiny_1_error
-@echo "OK: Flashed ATTiny"
-
-@goto :start
-
+:try_flash
+for /L %%x in (1, 1, 4) do (
+    bin\avrdude -v -pattiny88 -cusbtiny -u -V -B .5 -U flash:w:firmware/attiny88.hex:i && (
+        echo "OK: Flashed ATTiny"
+        goto main_loop
+    )
+    echo "Attempt %%x to flash ATTiny failed." >> attiny-log.txt
+)
+echo "Failed to flash ATTiny after 4 attempts." >> attiny-log.txt
+cscript bin\popup.vbs "FAIL: Flashing could not be completed"
+goto main_loop
 
 :wrong_chip
-@echo "BAD: Could not find GD32 chip."
-@echo "Could not find device GD32 Failed with error #%errorlevel%." >> gd32.log.txt
-@cscript bin\popup.vbs "BAD: Could not find GD32 chip"
-
-@goto :start
-
-
-
-
-
-goto :start
-
-:attiny_1_error
-echo "Failed with error #%errorlevel%." >> attiny-log.txt
-cscript bin\popup.vbs "FAIL: Check cable"
-goto :attiny_2
-
-:wrong_attiny_1
-echo "Could not find device Failed with error #%errorlevel%." >> attiny-log.txt
+echo "BAD: Could not find ATTiny88 chip."
+echo "Could not find device ATTiny88. Failed with error #%errorlevel%." >> attiny-log.txt
 cscript bin\popup.vbs "FAIL: ATTiny88 not found"
-goto :attiny_2
-
-
-:attiny_2_error
-echo "Failed with error #%errorlevel%." >> attiny-log.txt
-cscript bin\popup.vbs "FAIL: Check cable"
-goto :start
-
-
-:wrong_attiny_2
-echo "Could not find device Failed with error #%errorlevel%." >> attiny-log.txt
-cscript bin\popup.vbs "FAIL: ATTiny88 not found"
-goto :
-
-
+goto main_loop
